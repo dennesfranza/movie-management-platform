86925709d6d71b2cff6df3875fcf34f4adc8d4a9
Dennes Franza <dennesfranza@gmail.com>
2025-07-03 01:28:56 +0800

    add cors

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'movies',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'movie_project.urls'
@@ -138,3 +140,5 @@ REST_FRAMEWORK = {
 }
 
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CORS_ALLOW_ALL_ORIGINS = True  # For development only!
+CORS_ALLOW_CREDENTIALS = True  # Allow cookies to be sent with cross-origin requests
